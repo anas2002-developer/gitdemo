11fa898e63708ee33e269ba13ae8b2849ebc38b1
anas2002 <anas4112002@gmail.com>
2023-03-17 01:33:33 +0530

    Revert "sout used"
    This reverts commit d04351580a7b7c84e705a4625ac1a3f602851e6c.

--- app/src/main/java/com/anas/gitdemo2/MainActivity.java
+++ app/src/main/java/com/anas/gitdemo2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("line 1");
+        print("line 1");
     }
 }
